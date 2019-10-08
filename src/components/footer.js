804ba3d1e0c478cb364e9609064a56868b5452b5
Mertcan Akardere <makardere@ku.edu.tr>
2019-10-08 13:30:18 +0300

    remove contentful

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,6 +12,7 @@ const Footer = () => (
       <ul class="links">
         <li><a href="mailto:makardere@ku.edu.tr" target="_top">makardere@ku.edu.tr</a></li>
 
+
       </ul>
       <li><a href="https://github.com/MertcanAkardere?tab=repositories" target="_blank">GitHub</a></li>
       <li><a href="https://www.linkedin.com/in/makardere/"  target="_blank">LinkedIn</a></li>
