e48b68e7d93a1488dc3775afca65d31d4866a15d
Mertcan Akardere <makardere@ku.edu.tr>
2019-10-16 12:14:21 +0300

    cv page pdf support fix for mobile

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -8,9 +8,12 @@ const CVPage = () => {
   return (
       <Layout>
           <SEO title="CV" />
-          <embed src="https://res.cloudinary.com/chiv/image/upload/v1570492569/pdfs/CV_MertcanAkardere_dssgh5.pdf" width="100%" height="1300px" />
+          <object data="https://res.cloudinary.com/chiv/image/upload/v1570492569/pdfs/CV_MertcanAkardere_dssgh5.pdf" type="application/pdf" width="100%" height="1300px">
+            <embed src="https://res.cloudinary.com/chiv/image/upload/v1570492569/pdfs/CV_MertcanAkardere_dssgh5.pdf" type="application/pdf" />
+              <p>This browser does not support PDFs. Please download the PDF to view it: <a href="https://res.cloudinary.com/chiv/image/upload/v1570492569/pdfs/CV_MertcanAkardere_dssgh5.pdf">Download CV (PDF)</a>.</p>
+          </object>
       </Layout>
   )
 }
-
+//           <embed src="https://res.cloudinary.com/chiv/image/upload/v1570492569/pdfs/CV_MertcanAkardere_dssgh5.pdf" width="100%" height="1300px" />
 export default CVPage
